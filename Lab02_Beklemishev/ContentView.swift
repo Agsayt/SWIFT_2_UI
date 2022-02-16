21f1d48de32fd2abbe6e3ef4115a6a61282b780c
Agsayt <111ag@rambler.ru>
2022-02-16 03:52:17 +0300

    Initial Commit

---
+++ Lab02_Beklemishev/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab02_Beklemishev
+//
+//  Created by Admin on 16.02.2022.
+//  Copyright © 2022 G483. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab02_BeklemishevTests/Lab02_BeklemishevTests.swift
@@ -0,0 +1,34 @@
+//
+//  Lab02_BeklemishevTests.swift
+//  Lab02_BeklemishevTests
+//
+//  Created by Admin on 16.02.2022.
+//  Copyright © 2022 G483. All rights reserved.
+//
+
+import XCTest
+@testable import Lab02_Beklemishev
+
+class Lab02_BeklemishevTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
